d51fecaf6f9d45cebca5f1ffc435d15c1c0a965e
sarker25 <shahadathbinoy25@outlook.com>
2023-09-06 21:09:28 +0600

    added global errors middlewares

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const app = express();
-const applyMiddlewares = require("./src/middlewares");
+const { applyMiddlewares, errorHandler } = require("./src/middlewares");
+
+// global middlewares
 
 applyMiddlewares(app);
+
+// global error handler
+
+app.use(errorHandler);
 
 module.exports = app;
--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -1,7 +1,8 @@
 const express = require("express");
+const errorHandler = require("./errorHandlers");
 
 const applyMiddlewares = (app) => {
   app.use(express.json());
 };
 
-module.exports = applyMiddlewares;
+module.exports = { applyMiddlewares, errorHandler };
