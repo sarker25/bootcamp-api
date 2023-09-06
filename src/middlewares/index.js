6d49aeb2fb668b91aeb82b29122115420f929cda
sarker25 <shahadathbinoy25@outlook.com>
2023-09-06 21:36:43 +0600

    add asynchronous hadler of controller for hadling errors in asynchronously

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const errorHandler = require("./errorHandlers");
+const asyncHandler = require("./asyncHandler");
 
 const applyMiddlewares = (app) => {
   app.use(express.json());
 };
 
-module.exports = { applyMiddlewares, errorHandler };
+module.exports = { applyMiddlewares, errorHandler, asyncHandler };
